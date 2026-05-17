6445f048a629e73ef52f28bd9d170d781442f073
Deo <alfieharrylansdell@gmail.com>
2026-05-17 13:17:22 +0100

    Scarfo type shit

--- src/main/java/net/awt/AWTDevTeam.java
+++ src/main/java/net/awt/AWTDevTeam.java
@@ -12,7 +12,8 @@ public class AWTDevTeam {
     public static final UUID NATHAN = UUID.fromString("5ddf0693-9a1f-4f34-b29d-9daa1e4d0c2a");
     public static final UUID TARDISOWNER = UUID.fromString("a82ed141-f1c7-4e33-8f07-3b7e3d556ba8");
     public static final UUID COLOURFUL = UUID.fromString("33e35580-7404-497b-bf0f-6e735afd67cd");
-    public static final List<UUID> UUIDList = List.of(LEFT,DINO,DEO,RHAMNOUS,NATHAN,TARDISOWNER,COLOURFUL);
+    public static final UUID SCARFO = UUID.fromString("373565e5-2ce7-49d6-a2a9-7bc0b512a5b0");
+    public static final List<UUID> UUIDList = List.of(LEFT,DINO,DEO,RHAMNOUS,NATHAN,TARDISOWNER,COLOURFUL,SCARFO);
 
     public AWTDevTeam() {
     }
