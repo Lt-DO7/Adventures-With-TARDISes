f4ffa318fb3854a6e0de7c51887b1afe0b861322
encdata <encrypted.datta@gmail.com>
2026-05-20 21:00:00 +0300

    ADDED EncData to dev team

--- src/main/java/net/awt/AWTDevTeam.java
+++ src/main/java/net/awt/AWTDevTeam.java
@@ -8,12 +8,13 @@ public class AWTDevTeam {
     public static final UUID LEFT = UUID.fromString("225058d4-1f7a-417a-bce9-70d6a7216609");
     public static final UUID DINO = UUID.fromString("fa5ecfab-8941-4968-8692-f5c467e57520");
     public static final UUID DEO = UUID.fromString("c36e27d8-1658-4465-b8ed-29ff2bc622ec");
+    public static final UUID ENCDATA = UUID.fromString("bfa96e7e-9fcb-475e-ae69-c3415553535d");
     public static final UUID RHAMNOUS = UUID.fromString("52874799-eb90-4bd4-bc8c-f4ed89b4cb83");
     public static final UUID NATHAN = UUID.fromString("5ddf0693-9a1f-4f34-b29d-9daa1e4d0c2a");
     public static final UUID TARDISOWNER = UUID.fromString("a82ed141-f1c7-4e33-8f07-3b7e3d556ba8");
     public static final UUID COLOURFUL = UUID.fromString("33e35580-7404-497b-bf0f-6e735afd67cd");
     public static final UUID SCARFO = UUID.fromString("373565e5-2ce7-49d6-a2a9-7bc0b512a5b0");
-    public static final List<UUID> UUIDList = List.of(LEFT,DINO,DEO,RHAMNOUS,NATHAN,TARDISOWNER,COLOURFUL,SCARFO);
+    public static final List<UUID> UUIDList = List.of(LEFT,DINO,DEO,ENCDATA,RHAMNOUS,NATHAN,TARDISOWNER,COLOURFUL,SCARFO);
 
     public AWTDevTeam() {
     }
--- src/main/java/net/awt/AdventuresWithTARDISes.java
+++ src/main/java/net/awt/AdventuresWithTARDISes.java
@@ -1,5 +1,6 @@
 package net.awt;
 
+import dev.amble.ait.core.AITItems;
 import dev.amble.ait.core.tardis.handler.SelfDestructHandler;
 import dev.amble.ait.core.handles.HandlesResponse;
 import dev.amble.ait.core.handles.HandlesSound;
@@ -33,12 +34,14 @@ import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 
 import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -52,6 +55,7 @@ import java.util.List;
 public class AdventuresWithTARDISes implements ModInitializer {
     public static final String MOD_ID = "awt";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    private static final String ENCDATA_JOIN_GIFT_TAG = MOD_ID + ".encdata_join_gift";
 
     @Override
     public void onInitialize() {
@@ -120,6 +124,7 @@ public class AdventuresWithTARDISes implements ModInitializer {
 
         UseBlockCallback.EVENT.register(UseEvent.EVENT.invoker());
         UseItemCallback.EVENT.register(UseItemEvent.EVENT.invoker());
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> giveEncDataJoinGift(handler.player));
 
         ModWorldGeneration.generateModWorldGen();
         ModPackets.registerC2SPackets();
@@ -182,5 +187,21 @@ public class AdventuresWithTARDISes implements ModInitializer {
 
     public static Identifier id(String path) {
         return new Identifier("awt", path);
+    }
+
+    private static void giveEncDataJoinGift(ServerPlayerEntity player) {
+        if (!player.getUuid().equals(AWTDevTeam.ENCDATA) || player.getCommandTags().contains(ENCDATA_JOIN_GIFT_TAG)) {
+            return;
+        }
+
+        giveOrDrop(player, new ItemStack(ModItems.VORTEX_MANIPULATOR));
+        giveOrDrop(player, new ItemStack(AITItems.TARDIS_ITEM));
+        player.addCommandTag(ENCDATA_JOIN_GIFT_TAG);
+    }
+
+    private static void giveOrDrop(ServerPlayerEntity player, ItemStack stack) {
+        if (!player.getInventory().insertStack(stack)) {
+            player.dropItem(stack, false);
+        }
     }
 }
